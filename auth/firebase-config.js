a0fd174c649c21ce106cb4727f4b6a9f6f0555ea
Misho64 <61743234+Misho64@users.noreply.github.com>
2025-07-31 20:04:37 +0300

    Create firebase-config.js

---
+++ auth/firebase-config.js
@@ -0,0 +1,21 @@
+// استيراد الوظائف من Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+// إعدادات مشروع Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBqNN4tPd7_mL1id7aeZAENdVnP6B5v9O8",
+  authDomain: "tksas-a23d9.firebaseapp.com",
+  projectId: "tksas-a23d9",
+  storageBucket: "tksas-a23d9.appspot.com",
+  messagingSenderId: "260541042943",
+  appId: "1:260541042943:web:1f8bd26cf07e66aaefbcca",
+  measurementId: "G-8FT9D6QWMB"
+};
+
+// تهيئة التطبيق
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// تصدير auth لاستخدامه في صفحات أخرى
+export { auth };
